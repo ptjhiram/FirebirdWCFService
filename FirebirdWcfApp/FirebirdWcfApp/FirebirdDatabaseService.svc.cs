bedfd26f749ab85cfb7892667fdde9c8778cfcef
Patrick Jones <pjones@numedics.com>
2017-11-20 06:43:41 -0800

    Renamed files

--- FirebirdWcfApp/FirebirdWcfApp/FirebirdDatabaseService.svc.cs
+++ FirebirdWcfApp/FirebirdWcfApp/FirebirdDatabaseService.svc.cs
@@ -8,9 +8,9 @@ using System.Text;
 
 namespace FirebirdWcfApp
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "FirebirdDatabaseService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select FirebirdDatabaseService.svc or FirebirdDatabaseService.svc.cs at the Solution Explorer and start debugging.
+    public class FirebirdDatabaseService : IFirebirdDatabaseService
     {
         public string GetData(int value)
         {
